e268898c6db1cb5a1f6bd555a7e7de8f083173b0
Reid Spencer <reid@reactific.com>
2016-01-30 11:11:44 -0500

    Require 90% coverage from now on.

--- project/Settings.scala
+++ project/Settings.scala
@@ -96,7 +96,7 @@ object Settings extends AssetsSettings {
     namespaceReverseRouter := true,
     coverageFailOnMinimum := true,
     coverageExcludedPackages := classesIgnoredByScoverage,
-    coverageMinimum := 80,
+    coverageMinimum := 90,
     coverallsToken := Some("uoZrsbhbC0E2289tvwp3ISntZLH2yjwqX"),
     buildInfoObject := "ScrupalBuildInfo",
     buildInfoPackage := "scrupal.core",
